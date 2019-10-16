1a5b19f583a929140eda3c556ba8f9185d44abd7
angela.alves <angela.alves@polarising.com>
2019-10-16 16:53:57 +0100

    Spring Video
    Without testing

---
+++ Spring/championsacademy/src/test/java/com/spring/champions/championsacademy/ChampionsacademyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spring.champions.championsacademy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChampionsacademyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
