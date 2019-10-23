3c9430366946080c1bc4668116603a21bf13e6e5
Tiago <tiago.martins.santos@polarising.com>
2019-10-23 14:08:25 +0100

    modifications

--- Project-Champions/src/app/events/add-events/add-events.component.ts
+++ Project-Champions/src/app/events/add-events/add-events.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-events',
@@ -11,5 +12,7 @@ export class AddEventsComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  onSubmit(form:NgForm){
+    console.log(form.value)
+  }
 }
