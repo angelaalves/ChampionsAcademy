3006a7e32377a9a70a605f16fbcd7cfab1d99fb5
Carolina Martins <carolina.martins@polarising.com>
2019-10-23 14:18:49 +0100

    dgfvgb

--- Project-Champions/src/app/app-routing.module.ts
+++ Project-Champions/src/app/app-routing.module.ts
@@ -22,11 +22,6 @@ import { AddGuildListComponent } from './guild-list-start/guild-list/add-guild-l
 import { AddUserComponent } from './ancient-profile/ancient/add-user/add-user.component';
 import { GuildListComponent } from './guild-list-start/guild-list/guild-list.component';
 import { ReactiveFormsModule } from '@angular/forms';
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
-
 
 const appRoutes: Routes=[
     {path: '', redirectTo: '/login', pathMatch:'full'},
